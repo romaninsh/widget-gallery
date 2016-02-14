a488cd8048b3bbd8a9a6da6c1ed131d3f063fa24
Romans Malinovskis <romans@agiletoolkit.org>
2016-02-14 20:15:51 +0000

    fix refresh on default

--- frontend/public/js/widgets.js
+++ frontend/public/js/widgets.js
@@ -12,8 +12,8 @@ $(function(){
 		$('#inspector').addClass('expanded');
 		$(this).addClass('hidden');
         resizeUpdate();
-        document.ace.resize();
-        document.ace.renderer.updateFull();
+        document.cm.refresh();
+        //document.ace.renderer.updateFull();
 	});
 	$('#inspector-hide').click(function(){
 		$(this).parents('#inspector').removeClass('expanded');
